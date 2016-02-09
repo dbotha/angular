04ecb39243cd7ed4474a8136fa721dbd2f32ebf6
Deon Botha <deon@dbotha.com>
2016-02-09 22:19:12 +0000

    More changes

--- app/interview/controller.js
+++ app/interview/controller.js
@@ -10,37 +10,6 @@ angular.module('myApp.interview', ['ngRoute'])
         });
     }])
 
-    .factory('todoList', [function() {
-        var notes = [];
-        var todoList = {
-            getNotes: function() {
-                return notes;
-            },
-
-            addNote: function(note) {
-                notes.push(note);
-            },
-
-            removeNote: function(note) {
-                var index = notes.indexOf(note);
-                if (index >= 0) notes.splice(index, 1);
-
-            }
-        };
-
-        return todoList;
-    }])
-
-    .controller('ViewController', ['todoList', function(todoList) {
+    .controller('ViewController', [function() {
         var ctrl = this;
-        ctrl.notes = todoList.getNotes();
-
-        ctrl.addNote = function(note) {
-            todoList.addNote(note);
-            ctrl.newNote = "";
-        }
-
-        ctrl.removeNote = function(note) {
-            todoList.removeNote(note);
-        }
     }]);
