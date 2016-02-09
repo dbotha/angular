f9b465d6675dbad6cdec17e651397fa68680ed96
Deon Botha <deon@dbotha.com>
2016-02-09 21:52:08 +0000

    ToggleButton directive question

---
+++ app/interview/controller.js
@@ -0,0 +1,30 @@
+'use strict';
+
+angular.module('myApp.interview', ['ngRoute'])
+
+    .config(['$routeProvider', function($routeProvider) {
+        $routeProvider.when('/interview', {
+            templateUrl: 'interview/view.html',
+            controller: 'ViewController',
+            controllerAs: "ctrl"
+        });
+    }])
+
+    .directive("interviewToggleButton", function() {
+        return {
+            controller: ["$scope", function($scope) {
+                this.toggled = $scope.toggled;
+            }],
+            controllerAs: "ctrl",
+            scope: {
+                toggled: "="
+            },
+            templateUrl: 'interview/toggle_button_view.html'
+        };
+    })
+
+    .controller('ViewController', [function() {
+        var ctrl = this;
+        ctrl.message = 'Hello World';
+        ctrl.toggled = true;
+    }]);
---
+++ app/interview/controller_tests.js
@@ -0,0 +1,16 @@
+'use strict';
+
+describe('myApp.interview module', function() {
+
+  beforeEach(module('myApp.interview'));
+
+  describe('view1 controller', function(){
+
+    it('should ....', inject(function($controller) {
+      //spec body
+      var ctrl = $controller('ViewController');
+      expect(ctrl).toBeDefined();
+    }));
+
+  });
+});
